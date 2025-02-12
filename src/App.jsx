01b2f65d2a99e093bf5a56b7b69b789d214501c8
Daniel Wahome <danielwahome@proton.me>
2025-02-12 13:55:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Navbar from './components/navbar/navbar';
+import Hero from './components/hero/Hero';
+import Programs from './components/programs/Programs';
+import Title from './components/title/Title';
+import About from './components/about/About';
+import Campus from './components/campus/Campus';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import VideoPlayer from './components/videoplayer/VideoPlayer';
+
+
+const App = () => {
+  
+  const [playState, setPlayState]=useState(false);
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our Programs' title='What we offer'/>
+        <Programs/>
+        <About setPlayState={setPlayState}/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='Testimonials' title='What our students say'/>
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in touch'/>
+        <Contact/>
+        <Footer/>
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+
+    const [result, setResult] = React.useState('');
+      
+        const onSubmit = async (event) => {
+          event.preventDefault();
+          setResult("Sending...");
+          const formData = new FormData(event.target);
+      
+          formData.append("access_key", "d3c9b32e-ac33-48d9-bc13-88249cf2de00");
+      
+          const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+          });
+      
+          const data = await response.json();
+      
+          if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+          } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }   
+    };
+
+    return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>Feel free to reach out through contact form or find our contact information below.
+                Your feedback, questions, and suggestions are important to us as we strive to provide
+                exceptional service to our university community.</p>
+            <ul>
+                <li><img src={mail_icon} alt="" /> Contact;GreatStack.dev </li>
+                <li> <img src={phone_icon} alt="" /> +1 123-456-7890</li>
+                <li><img src={location_icon} alt="" /> 77 Massachusetts Ave, Cambridge<br/> MA 02139, United States</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your Name</label>
+                <input type="text" name='name' placeholder='Enter Your Name' required />
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter Phone Number' required />
+                <label>Write your message</label>
+                <textarea name="message" rows='6' placeholder='Enter Your Message'></textarea>
+                <button type='submit' className='btn dark-btn'>Submit
+                    <img src={white_arrow} alt="" />
+                </button>
+            </form>
+            <span>{result}</span>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
